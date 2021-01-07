0f32fb670c3d06be16ec09a3f846026fcbcb0dbb
Jason Lovell <jasonlovelluk@outlook.com>
2021-01-07 17:14:55 +0000

    updated js

---
+++ JS/van.js
@@ -0,0 +1,6 @@
+document.getElementById("testOne").innerHTML="Hi there from JS";
+
+function msgMe() {
+    document.getElementById("testOne").innerHTML="Button pressed";
+     alert("Hello again from JS");
+}
